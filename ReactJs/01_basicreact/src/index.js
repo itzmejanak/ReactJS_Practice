1b0a2e91335060343f4ce503a6da37cbb2eef300
Janak Devkota <itzmejanak@gmail.com>
2024-02-02 22:52:10 +0545

    🎉 init:

--- ReactJs/01_basicreact/src/index.js
+++ ReactJs/01_basicreact/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+//react dom is the implementation of the web same like reactNative is the implementation of the mobile apps
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
@@ -10,6 +11,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
